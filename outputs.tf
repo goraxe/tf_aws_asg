74100492fb82ec7e05006284fdc5a56dd0f160db
smotti <wottis@gmail.com>
2016-11-18 16:54:37 +0800

    Add the ASG name to outputs

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,7 @@ output "launch_config_id" {
 output "asg_id" {
     value = "${aws_autoscaling_group.main_asg.id}"
 }
+
+output "asg_name" {
+    value = "${aws_autoscaling_group.main_asg.name}"
+}
